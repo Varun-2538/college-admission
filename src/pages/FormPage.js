ef96bbb962bada29048fd5402ddb510b9d8e4ec8
Varun-2538 <942525@gmail.com>
2024-08-11 22:01:03 +0530

    states name added in formpage

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -85,21 +85,49 @@ const FormPage = () => {
               required 
             >
               <option value="">Select State</option>
-              <option value="State1">State 1</option>
-              <option value="State2">State 2</option>
-              {/* Add more options as needed */}
+              <option value="Andhra Pradesh">Andhra Pradesh</option>
+              <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+              <option value="Assam">Assam</option>
+              <option value="Bihar">Bihar</option>
+              <option value="Chhattisgarh">Chhattisgarh</option>
+              <option value="Goa">Goa</option>
+              <option value="Gujarat">Gujarat</option>
+              <option value="Haryana">Haryana</option>
+              <option value="Himachal Pradesh">Himachal Pradesh</option>
+              <option value="Jharkhand">Jharkhand</option>
+              <option value="Karnataka">Karnataka</option>
+              <option value="Kerala">Kerala</option>
+              <option value="Madhya Pradesh">Madhya Pradesh</option>
+              <option value="Maharashtra">Maharashtra</option>
+              <option value="Manipur">Manipur</option>
+              <option value="Meghalaya">Meghalaya</option>
+              <option value="Mizoram">Mizoram</option>
+              <option value="Nagaland">Nagaland</option>
+              <option value="Odisha">Odisha</option>
+              <option value="Punjab">Punjab</option>
+              <option value="Rajasthan">Rajasthan</option>
+              <option value="Sikkim">Sikkim</option>
+              <option value="Tamil Nadu">Tamil Nadu</option>
+              <option value="Telangana">Telangana</option>
+              <option value="Tripura">Tripura</option>
+              <option value="Uttar Pradesh">Uttar Pradesh</option>
+              <option value="Uttarakhand">Uttarakhand</option>
+              <option value="West Bengal">West Bengal</option>
+              <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+              <option value="Chandigarh">Chandigarh</option>
+              <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
+              <option value="Lakshadweep">Lakshadweep</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Puducherry">Puducherry</option>
             </select>
-            <select 
+            <input 
+              type="text" 
               name="city" 
+              placeholder="Enter City" 
               className="w-1/2 p-2 border rounded" 
               onChange={handleChange}
               required 
-            >
-              <option value="">Select City</option>
-              <option value="City1">City 1</option>
-              <option value="City2">City 2</option>
-              {/* Add more options as needed */}
-            </select>
+            />
           </div>
           <div className="mb-4">
             <input 
