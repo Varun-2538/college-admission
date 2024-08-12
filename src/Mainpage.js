66ab1c610f9dd3be7045997ac5a891dc68634608
imilindmishra <milindmishra11@gmail.com>
2024-08-13 04:17:46 +0530

    Refactor: mainpage

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -1,27 +1,63 @@
-import React from 'react';
-import './index.css'; // Ensure Tailwind CSS is imported
-import CarouselPage from './pages/CarouselPage';
-import FormPage from './pages/FormPage';
-import CoursesPage from './components/CoursePage';
-import Navbar from './components/Navbar';
+import React from "react";
+import "./index.css"; // Ensure Tailwind CSS is imported
+import CarouselPage from "./pages/CarouselPage";
+import FormPage from "./pages/FormPage";
+import CoursesPage from "./components/CoursePage";
+import Navbar from "./components/Navbar";
+import FloatingButton from "./components/FloatingButton";
 
 const MainPage = () => {
+  const scrollToFormPage = () => {
+    const formPage = document.getElementById("formPage");
+    if (formPage) {
+      formPage.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="relative">
       <Navbar />
-      <div className="min-h-screen bg-college-bg bg-cover bg-center flex flex-col items-center justify-start text-center pt-60">
-        <h1 className="text-4xl text-white font-bold animate-slideDown mt-[-48px]">
+      <div className="min-h-screen bg-college-bg bg-cover bg-center flex flex-col items-center justify-start text-center pt-20">
+        <h1 className="text-4xl text-black font-bold animate-slideDown mt-8 font-sans">
           Welcome to Admissions of Our Colleges
         </h1>
-        <div className="flex justify-start items-start w-full px-10">
-          <div className="text-left w-full animate-slideInLeft">
-            <p className="text-3xl font-bold text-white mt-4">Don't Worry<br/>For 12th Marks</p>
-            {/* <p className="text-4xl font-bold bg-opacity-50 rounded-lg bg-gray-700 text-white mt-4">Less Then 60% Marks In PCM</p> */}
+        <p
+          className="text-2xl font-bold text-white bg-blue-600 px-4 py-2 rounded-lg shadow-lg border-2 border-white mt-4 animate-pulse cursor-pointer"
+          onClick={scrollToFormPage}
+        >
+          Admission Open 2024-25
+        </p>
+        <div className="flex justify-start w-full px-10">
+          <div className="text-left w-full ">
+            <p className="text-3xl text-black font-bold mt-24 ml-60">
+              Don't Worry For 12th Marks
+            </p>
+            <p className="text-2xl font-bold text-blue-800 mt-4 ml-60">
+              Explore Our UG & PG Program
+            </p>
+            <div className="flex flex-row  place-content-evenly mt-12 ml-60 mr-60">
+              <FloatingButton
+                name="Engineering"
+                className="mt-4"
+                onClick={scrollToFormPage}
+              />
+              <FloatingButton
+                name="Medical"
+                className="mt-4"
+                onClick={scrollToFormPage}
+              />
+              <FloatingButton
+                name="Law"
+                className="mt-4"
+                onClick={scrollToFormPage}
+              />
+              <FloatingButton
+                name="Management"
+                className="mt-4"
+                onClick={scrollToFormPage}
+              />
+            </div>
           </div>
-        </div>
-        <div className="w-full flex flex-col items-center mt-80">
-          <p className="text-2xl font-bold text-white">Explore Our<br/>UG & PG Program</p>
-          <p className="text-2xl font-bold text-blue mt-4">Admission Open 2024-25</p>
         </div>
       </div>
 
